a09fdd53f6590ad168bc8d1917128a985784a56d
L.Balaguru <117264451+Balaguru0414@users.noreply.github.com>
2023-09-14 16:17:30 +0530

    Update UseEffect.jsx

--- src/Hooks/UseEffect.jsx
+++ src/Hooks/UseEffect.jsx
@@ -1,22 +1,42 @@
 import React, { useEffect, useState } from "react";
 
 const UseEffect = () => {
-  const [resource, setResource] = useState("posts");
-  console.log("before renderd");
+  const [width, setWidth] = useState(window.innerWidth);
   useEffect(() => {
-    console.log("renderd");
-
-    return () => console.log("i am from return");
-  }, [resource]);
-
-  return (
-    <div>
-      <button onClick={() => setResource("posts")}>Posts</button>
-      <button onClick={() => setResource("users")}>Users</button>
-      <button onClick={() => setResource("comments")}>Comments</button>
-      <h1>{resource}</h1>
-    </div>
-  );
+    window.addEventListener("resize", updateWidth);
+    console.log("from useeffect");
+    return () => {
+      console.log("from return useeffect");
+      window.removeEventListener("resize", updateWidth);
+    };
+  });
+  const updateWidth = () => {
+    setWidth(window.innerWidth);
+  };
+  return <h2>{width}</h2>;
+  //
+  //
+  //
+    const [resource, setResource] = useState("posts");
+    const [item, setItem] = useState([]);
+    console.log("before renderd");
+    useEffect(() => {
+      fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+        .then((response) => response.json())
+        .then((json) => setItem(json));
+      return () => console.log("i am from return");
+    }, [resource]);
+    return (
+      <div>
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("users")}>Users</button>
+        <button onClick={() => setResource("comments")}>Comments</button>
+        <h1>{resource}</h1>
+        {item.map((el) => (
+          <p>{JSON.stringify(el)}</p>
+        ))}
+      </div>
+    );
 };
 
 export default UseEffect;
