7b190ae32a499f45e988fc68915b30df9bd254fb
L.Balaguru <117264451+Balaguru0414@users.noreply.github.com>
2023-09-28 16:27:29 +0530

    Update UseContext.jsx

--- src/Hooks/useContext/UseContext.jsx
+++ src/Hooks/useContext/UseContext.jsx
@@ -1,87 +1,87 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// export default function UseContext() {
-//   const [name, setName] = useState("Bala");
-//   return (
-//     <div>
-//       <p>name : {name}</p>
-//       <One setName={setName} />
-//     </div>
-//   );
-// }
+export default function UseContext() {
+  const [name, setName] = useState("Bala");
+  return (
+    <div>
+      <p>name : {name}</p>
+      <One setName={setName} />
+    </div>
+  );
+}
 
-// const One = ({ setName }) => {
-//   return (
-//     <>
-//       <p>From One</p>
-//       <Two setName={setName} />
-//     </>
-//   );
-// };
+const One = ({ setName }) => {
+  return (
+    <>
+      <p>From One</p>
+      <Two setName={setName} />
+    </>
+  );
+};
 
-// const Two = ({ setName }) => {
-//   return (
-//     <>
-//       <p>From Two</p>
-//       <Three setName={setName} />
-//     </>
-//   );
-// };
+const Two = ({ setName }) => {
+  return (
+    <>
+      <p>From Two</p>
+      <Three setName={setName} />
+    </>
+  );
+};
 
-// const Three = ({ setName }) => {
-//   return (
-//     <>
-//       <p>From Two</p>
-//       <button onClick={() => setName("L.Balaguru")}>Change Name</button>
-//     </>
-//   );
-// };
+const Three = ({ setName }) => {
+  return (
+    <>
+      <p>From Two</p>
+      <button onClick={() => setName("L.Balaguru")}>Change Name</button>
+    </>
+  );
+};
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-// import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-// const AppContext = createContext(null);
+const AppContext = createContext(null);
 
-// export default function UseContext() {
-//   const [name, setName] = useState("Bala");
-//   return (
-//     <AppContext.Provider value={setName}>
-//       <div>
-//         <p>name : {name}</p>
-//         <One />
-//       </div>
-//     </AppContext.Provider>
-//   );
-// }
+export default function UseContext() {
+  const [name, setName] = useState("Bala");
+  return (
+    <AppContext.Provider value={setName}>
+      <div>
+        <p>name : {name}</p>
+        <One />
+      </div>
+    </AppContext.Provider>
+  );
+}
 
-// const One = () => {
-//   return (
-//     <>
-//       <p>From One</p>
-//       <Two />
-//     </>
-//   );
-// };
+const One = () => {
+  return (
+    <>
+      <p>From One</p>
+      <Two />
+    </>
+  );
+};
 
-// const Two = () => {
-//   return (
-//     <>
-//       <p>From Two</p>
-//       <Three />
-//     </>
-//   );
-// };
+const Two = () => {
+  return (
+    <>
+      <p>From Two</p>
+      <Three />
+    </>
+  );
+};
 
-// const Three = () => {
-//   const setName = useContext(AppContext);
-//   return (
-//     <>
-//       <p>From Two</p>
-//       <button onClick={() => setName("L.Balaguru")}>Change Name</button>
-//     </>
-//   );
-// };
+const Three = () => {
+  const setName = useContext(AppContext);
+  return (
+    <>
+      <p>From Two</p>
+      <button onClick={() => setName("L.Balaguru")}>Change Name</button>
+    </>
+  );
+};
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -100,3 +100,42 @@ export default function UseContext() {
     </NameContext.Provider>
   );
 }
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+import React, { createContext, useContext, useState } from "react";
+
+const AppContext = createContext(null);
+const ThemeContext = createContext(null);
+
+export default function UseContext() {
+  const [name, setName] = useState("Bala");
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <AppContext.Provider value={setName}>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <div>
+          <p>name: {name}</p>
+          <p>theme: {theme}</p>
+          <One />
+        </div>
+      </ThemeContext.Provider>
+    </AppContext.Provider>
+  );
+}
+
+const Three = () => {
+  const setName = useContext(AppContext);
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return (
+    <>
+      <p>From Three</p>
+      <p>Current Theme: {theme}</p>
+      <button onClick={() => setTheme("Dark")}>Change Name</button>
+      <button onClick={() => setName("L.Balaguru")}>Change Name</button>
+    </>
+  );
+};
