4fb0a42f61366563764e3fca873c8da87ea226b1
L.Balaguru <117264451+Balaguru0414@users.noreply.github.com>
2023-11-04 11:58:09 +0530

    Create basic.jsx

---
+++ src/basic.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+// create component
+function App() {
+  return <h1>Hello World</h1>;
+}
+// create with state component
+function App() {
+  const [state, setState] = useState("Hello World");
+  const [style, setStyle] = useState();
+
+  return (
+    <div>
+      <h1 style={style}>{state}</h1>
+      <button
+        onClick={() => {
+          setState("Hello everyone");
+          setStyle({
+            background: "red",
+            color: "yellow",
+          });
+        }}
+      >
+        Change
+      </button>
+    </div>
+  );
+}
